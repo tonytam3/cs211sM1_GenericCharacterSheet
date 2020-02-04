9a0179210466168d73aef173ad1bcd2db1fc3e31
TT Main <ttam8@mail.ccsf.edu>
2020-02-03 20:48:31 -0800

    getter and setter for stats updated

--- src/GenericCharacter.java
+++ src/GenericCharacter.java
@@ -1,12 +1,30 @@
 
 public abstract class GenericCharacter {
+	
+	/*
+	 * Parent Class for a Generic Character Sheet. 
+	 * Most table top role playing systems have numerous differences,
+	 * but the what constitutes a "character" in these systems are
+	 * similar in ideas with different implementations 
+	 * 
+	 * For example, a character's name, it's background,
+	 * it's attributes or stats, and the list goes on.
+	 * 
+	 */
 
 	private String Name;
 	private StatNode[] statNodeArray;
-
+	
+	/*
+	 * Constructor for GenericCharacter
+	 */
 	public GenericCharacter( int statNodeArraySize) {
 		statNodeArray = new StatNode[statNodeArraySize];
 	}
+	
+	/*
+	 * Getters and Setters
+	 */
 
 	public String getName() {
 		return Name;
@@ -15,18 +33,62 @@ public abstract class GenericCharacter {
 	public void setName(String name) {
 		Name = name;
 	}
+	
+	public StatNode[] getStat() {
+		return statNodeArray;
+	}
+	
+	public void setStatNode (int nodePosition, String statName) {
+		if (nodePosition < statNodeArray.length) {
+			this.statNodeArray[nodePosition].setStatName(statName);
+		} else {
+			System.out.print("index out of bound");
+		}
+	}
+	
+	/*
+	 * Prints out the name of stat and value of the stat
+	 * when the array position is valid.
+	 */
 
 	public void readStatNode(int nodePosition) {
 		if (nodePosition < statNodeArray.length) {
-			System.out.println(statNodeArray[nodePosition]);
+			System.out.println(statNodeArray[nodePosition].getStatName() + ": "+
+					statNodeArray[nodePosition].getStatValue());
 		} else {
 			System.out.print("Index out of Bounds");
 		}
 	}
+	
+	/*
+	 * returns a name.. trying to think of reason why I would
+	 * want this here
+	 */
 
 	@Override
 	public String toString() {
-		return Name ;
+		return ("Name of the character: " + Name);
 	}
+	
+	/*
+	 *I'm leaving this here, but honestly the equals method should be
+	 *implemented on the child class
+	 */
+	
+	@Override
+	public boolean equals(Object obj) {
+		if((GenericCharacter) obj instanceof GenericCharacter) {
+			GenericCharacter temp = (GenericCharacter) obj;
+			if(temp.Name.equals(this.Name) &&  temp.statNodeArray.equals(this.statNodeArray)) {
+				return true;
+			}else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	
+	
 
 }
